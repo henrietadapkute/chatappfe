ffe06147dac5c1c8af5fa415ac368bca3856adb5
henrietadapkute <eimante.dapkute@gmail.com>
2024-02-01 17:53:32 +0000

    Basic frontend setup

---
+++ src/components/ChatListView/ChatListView.jsx
@@ -0,0 +1,7 @@
+// List of conversations available
+export default function ChatListView() {
+    return (
+      <div>ChatListView</div>
+    )
+  }
+  
---
+++ src/components/SendMessageForm/SendMessageForm.jsx
@@ -0,0 +1,6 @@
+/// ui for sending a new message 
+export default function SendMessageForm() {
+  return (
+    <div>SendMessageForm</div>
+  )
+}
